d1f522e90e2d6f4ed6de21e65a66bcdd9de2aca9
neotusca <neotusca@gmail.com>
2017-02-08 11:00:07 +0900

    httpd modify

--- web-httpd/config-data/push.sh
+++ web-httpd/config-data/push.sh
@@ -2,7 +2,14 @@
 
 
 
-#scp httpd-conf.tar c72-k8s-node1:/data/HTTPD_DIR/CONF/
-#scp httpd-conf.tar c72-k8s-node2:/data/HTTPD_DIR/CONF/
-#scp httpd-conf.tar c72-k8s-node3:/data/HTTPD_DIR/CONF/
-scp httpd-conf.tar c72-k8s-node4:/data/HTTPD_DIR/CONF/
+#scp httpd-conf.tar         c72-k8s-node1:/data/HTTPD_DIR/CONF/
+#scp garbage_collection.sh  c72-k8s-node1:/data/HTTPD_DIR/CONF/
+
+#scp httpd-conf.tar         c72-k8s-node2:/data/HTTPD_DIR/CONF/
+#scp garbage_collection.sh  c72-k8s-node2:/data/HTTPD_DIR/CONF/
+
+#scp httpd-conf.tar         c72-k8s-node3:/data/HTTPD_DIR/CONF/
+#scp garbage_collection.sh  c72-k8s-node3:/data/HTTPD_DIR/CONF/
+
+#scp httpd-conf.tar         c72-k8s-node4:/data/HTTPD_DIR/CONF/
+#scp garbage_collection.sh  c72-k8s-node4:/data/HTTPD_DIR/CONF/
