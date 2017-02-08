a07f25c4d3a56115ea5bb052940337e2f491908e
neotusca <neotusca@gmail.com>
2017-02-08 17:46:48 +0900

    modify tomcat config-data

--- was-tomcat/build/start-tomcat.sh
+++ was-tomcat/build/start-tomcat.sh
@@ -5,8 +5,8 @@ docker run -d -p 8888:8080 \
            -v /data/TOMCAT_DIR/CONF:/usr/local/tomcat/conf \
            -v /data/TOMCAT_DIR/DATA:/usr/local/tomcat/webapps \
            -v /data/TOMCAT_DIR/LOG:/usr/local/tomcat/logs \
-           tomcat:8.5-alpine-final
-           #tomcat:8.0-alpine-final
+           192.168.254.20:5000/tomcat:8.5-alpine-final
+           #tomcat:8.5-alpine-final
 
 
 sleep 2
--- was-tomcat/config-data/push.sh
+++ was-tomcat/config-data/push.sh
@@ -2,19 +2,23 @@
 
 
 
-#scp -pr tomcat-conf/* c72-k8s-node1:/data/TOMCAT_DIR/CONF/
-#scp -pr tomcat-webapps/* c72-k8s-node1:/data/TOMCAT_DIR/DATA/
-#scp ROOT.war          c72-k8s-node1:/data/TOMCAT_DIR/DATA/
+#ssh c72-k8s-node1 'mkdir -p /data/TOMCAT_DIR/CONF/origin'
+#scp -pr tomcat-conf/*    c72-k8s-node1:/data/TOMCAT_DIR/CONF/origin
+#scp -pr ROOT.war         c72-k8s-node1:/data/TOMCAT_DIR/DATA/
+#scp -pr garbage_collection.sh c72-k8s-node1:/data/TOMCAT_DIR
 
-#scp -pr tomcat-conf/* c72-k8s-node2:/data/TOMCAT_DIR/CONF/
-#scp -pr tomcat-webapps/* c72-k8s-node2:/data/TOMCAT_DIR/DATA/
-#scp ROOT.war          c72-k8s-node2:/data/TOMCAT_DIR/DATA/
+#ssh c72-k8s-node2 'mkdir -p /data/TOMCAT_DIR/CONF/origin'
+#scp -pr tomcat-conf/*    c72-k8s-node2:/data/TOMCAT_DIR/CONF/origin
+#scp -pr ROOT.war         c72-k8s-node2:/data/TOMCAT_DIR/DATA/
+#scp -pr garbage_collection.sh c72-k8s-node2:/data/TOMCAT_DIR
 
-#scp -pr tomcat-conf/* c72-k8s-node3:/data/TOMCAT_DIR/CONF/
-#scp -pr tomcat-webapps/* c72-k8s-node3:/data/TOMCAT_DIR/DATA/
-#scp ROOT.war          c72-k8s-node3:/data/TOMCAT_DIR/DATA/
+#ssh c72-k8s-node3 'mkdir -p /data/TOMCAT_DIR/CONF/origin'
+#scp -pr tomcat-conf/*    c72-k8s-node3:/data/TOMCAT_DIR/CONF/origin
+#scp -pr ROOT.war         c72-k8s-node3:/data/TOMCAT_DIR/DATA/
+#scp -pr garbage_collection.sh c72-k8s-node3:/data/TOMCAT_DIR
 
-#scp -pr tomcat-conf/* c72-k8s-node4:/data/TOMCAT_DIR/CONF/
-#scp -pr tomcat-webapps/* c72-k8s-node4:/data/TOMCAT_DIR/DATA/
-#scp ROOT.war          c72-k8s-node4:/data/TOMCAT_DIR/DATA/
+#ssh c72-k8s-node4 'mkdir -p /data/TOMCAT_DIR/CONF/origin'
+#scp -pr tomcat-conf/*    c72-k8s-node4:/data/TOMCAT_DIR/CONF/origin
+#scp -pr ROOT.war         c72-k8s-node4:/data/TOMCAT_DIR/DATA/
+#scp -pr garbage_collection.sh c72-k8s-node4:/data/TOMCAT_DIR
 
