9b22ec6ca303511bfe1b8c0e410317ddb97a46c7
neotusca <neotusca@gmail.com>
2017-02-08 14:21:40 +0900

    tomcat modify

--- was-tomcat/config-data/push.sh
+++ was-tomcat/config-data/push.sh
@@ -2,10 +2,19 @@
 
 
 
-#scp tomcat-*.tar c72-k8s-node1:/data/TOMCAT_DIR/CONF/
-#scp tomcat-*.tar c72-k8s-node2:/data/TOMCAT_DIR/CONF/
-#scp tomcat-*.tar c72-k8s-node3:/data/TOMCAT_DIR/CONF/
+#scp -pr tomcat-conf/* c72-k8s-node1:/data/TOMCAT_DIR/CONF/
+#scp -pr tomcat-webapps/* c72-k8s-node1:/data/TOMCAT_DIR/DATA/
+#scp ROOT.war          c72-k8s-node1:/data/TOMCAT_DIR/DATA/
 
-scp tomcat-*.tar c72-k8s-node4:/data/TOMCAT_DIR/CONF/
-scp ROOT.war     c72-k8s-node4:/data/TOMCAT_DIR/DATA/
+#scp -pr tomcat-conf/* c72-k8s-node2:/data/TOMCAT_DIR/CONF/
+#scp -pr tomcat-webapps/* c72-k8s-node2:/data/TOMCAT_DIR/DATA/
+#scp ROOT.war          c72-k8s-node2:/data/TOMCAT_DIR/DATA/
+
+#scp -pr tomcat-conf/* c72-k8s-node3:/data/TOMCAT_DIR/CONF/
+#scp -pr tomcat-webapps/* c72-k8s-node3:/data/TOMCAT_DIR/DATA/
+#scp ROOT.war          c72-k8s-node3:/data/TOMCAT_DIR/DATA/
+
+#scp -pr tomcat-conf/* c72-k8s-node4:/data/TOMCAT_DIR/CONF/
+#scp -pr tomcat-webapps/* c72-k8s-node4:/data/TOMCAT_DIR/DATA/
+#scp ROOT.war          c72-k8s-node4:/data/TOMCAT_DIR/DATA/
 
