d2b054c7c41ae2a6e336f3fac74f5e1a3cc9bb19
neotusca <neotusca@gmail.com>
2017-02-09 20:37:48 +0900

    modify web-httpd, web-nginx

--- web-httpd/config-data/push.sh
+++ web-httpd/config-data/push.sh
@@ -2,14 +2,18 @@
 
 
 
-#scp httpd-conf.tar         c72-k8s-node1:/data/HTTPD_DIR/CONF/
-#scp garbage_collection.sh  c72-k8s-node1:/data/HTTPD_DIR/CONF/
+#scp conf/*                 c72-k8s-node1:/data/HTTPD_DIR/CONF/
+#scp htdocs/index.html      c72-k8s-node1:/data/HTTPD_DIR/DATA/
+#scp garbage_collection.sh  c72-k8s-node1:/data/HTTPD_DIR
 
-#scp httpd-conf.tar         c72-k8s-node2:/data/HTTPD_DIR/CONF/
-#scp garbage_collection.sh  c72-k8s-node2:/data/HTTPD_DIR/CONF/
+#scp conf/*                 c72-k8s-node2:/data/HTTPD_DIR/CONF/
+#scp htdocs/index.html      c72-k8s-node2:/data/HTTPD_DIR/DATA/
+#scp garbage_collection.sh  c72-k8s-node2:/data/HTTPD_DIR
 
-#scp httpd-conf.tar         c72-k8s-node3:/data/HTTPD_DIR/CONF/
-#scp garbage_collection.sh  c72-k8s-node3:/data/HTTPD_DIR/CONF/
+#scp conf/*                 c72-k8s-node3:/data/HTTPD_DIR/CONF/
+#scp htdocs/index.html      c72-k8s-node3:/data/HTTPD_DIR/DATA/
+#scp garbage_collection.sh  c72-k8s-node3:/data/HTTPD_DIR
 
-#scp httpd-conf.tar         c72-k8s-node4:/data/HTTPD_DIR/CONF/
-#scp garbage_collection.sh  c72-k8s-node4:/data/HTTPD_DIR/CONF/
+#scp conf/*                 c72-k8s-node4:/data/HTTPD_DIR/CONF/
+#scp htdocs/index.html      c72-k8s-node4:/data/HTTPD_DIR/DATA/
+#scp garbage_collection.sh  c72-k8s-node4:/data/HTTPD_DIR
--- web-nginx/config-data/push.sh
+++ web-nginx/config-data/push.sh
@@ -1,17 +1,16 @@
 
+#scp -pr nginx-conf/*           c72-k8s-node1:/data/NGINX_DIR/CONF/
+#scp -pr nginx-html/*           c72-k8s-node1:/data/NGINX_DIR/DATA/
+#scp -pr garbage_collection.sh  c72-k8s-node1:/data/NGINX_DIR
 
+#scp -pr nginx-conf/*           c72-k8s-node2:/data/NGINX_DIR/CONF/
+#scp -pr nginx-html/*           c72-k8s-node2:/data/NGINX_DIR/DATA/
+#scp -pr garbage_collection.sh  c72-k8s-node2:/data/NGINX_DIR
 
+#scp -pr nginx-conf/*           c72-k8s-node3:/data/NGINX_DIR/CONF/
+#scp -pr nginx-html/*           c72-k8s-node3:/data/NGINX_DIR/DATA/
+#scp -pr garbage_collection.sh  c72-k8s-node3:/data/NGINX_DIR
 
-#scp nginx-*.tar            c72-k8s-node1:/data/NGINX_DIR/CONF/
-#scp garbage_collection.sh  c72-k8s-node1:/data/NGINX_DIR
-
-#scp nginx-*.tar            c72-k8s-node2:/data/NGINX_DIR/CONF/
-#scp garbage_collection.sh  c72-k8s-node2:/data/NGINX_DIR
-
-#scp nginx-*.tar            c72-k8s-node3:/data/NGINX_DIR/CONF/
-#scp garbage_collection.sh  c72-k8s-node3:/data/NGINX_DIR
-
-#scp nginx-*.tar            c72-k8s-node4:/data/NGINX_DIR/CONF/
-#scp garbage_collection.sh  c72-k8s-node4:/data/NGINX_DIR
-
-
+#scp -pr nginx-conf/*           c72-k8s-node4:/data/NGINX_DIR/CONF/
+#scp -pr nginx-html/*           c72-k8s-node4:/data/NGINX_DIR/DATA/
+#scp -pr garbage_collection.sh  c72-k8s-node4:/data/NGINX_DIR
